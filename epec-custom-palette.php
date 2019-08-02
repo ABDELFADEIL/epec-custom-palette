96e180197c9d6745a4ecfe090a9f86242cb48b04
ABDELFADEIL <abdalfadeil@gmail.com>
2019-08-02 10:34:04 +0200

    Traduction des couleurs du plugin

--- epec-custom-palette.php
+++ epec-custom-palette.php
@@ -14,22 +14,22 @@ License: GPL2
 function mytheme_setup_theme_supported_features() {
     add_theme_support( 'editor-color-palette', array(
         array(
-            'name' => __( 'Brand Color 1', 'themeLangDomain' ),
+            'name' => __( 'Brand Color 1', 'epec-custom-palette' ),
             'slug' => 'strong-magenta',
             'color' => '#6128A1',
         ),
         array(
-            'name' => __( 'Brand Color 2', 'themeLangDomain' ),
-            'slug' => 'red',
+            'name' => __( 'Brand Color 2', 'epec-custom-palette' ),
+            'slug' => 'violet',
             'color' => '#E94B59',
         ),
         array(
-            'name' => __( 'black', 'themeLangDomain' ),
+            'name' => __( 'black', 'epec-custom-palette' ),
             'slug' => 'black',
             'color' => '#111111',
         ),
         array(
-            'name' => __( 'white', 'themeLangDomain' ),
+            'name' => __( 'white', 'epec-custom-palette' ),
             'slug' => 'white',
             'color' => 'White',
         ),
@@ -39,4 +39,11 @@ function mytheme_setup_theme_supported_features() {
 }
  
   add_action( 'after_setup_theme', 'mytheme_setup_theme_supported_features' );
+
+function my_scripts() {
+wp_enqueue_script( 'jquery' );
+wp_register_style( 'prefix-style', plugins_url('style.css', __FILE__) );
+wp_enqueue_style( 'prefix-style' );
+}
+add_action('wp_enqueue_scripts','my_scripts');
 
